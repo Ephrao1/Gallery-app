fac5a0a6604e6bfd144be2e0bc2d3ca6cc69ebb5
Ephraim Bundi <ebundi04@gmail.com>
2021-10-11 18:47:17 +0300

    Make changes

--- photoslog/admin.py
+++ photoslog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Image,Location,Category
+from .models import Image,Location,Category,photos
 
 
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Category)
 admin.site.register(Location)
-
+admin.site.register(photos)

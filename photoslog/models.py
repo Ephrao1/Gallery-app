c4524b8fce26270b0f3c9a5f0760cbe95827b9b7
Ephraim Bundi <ebundi04@gmail.com>
2021-10-13 12:22:30 +0300

    Initial commit

--- photoslog/models.py
+++ photoslog/models.py
@@ -57,7 +57,7 @@ class Image(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
     Image = models.ImageField(upload_to='photos/') 
-    image = CloudinaryField('image')
+    # image = CloudinaryField('image')
     Image_location = models.ForeignKey('Location', on_delete=models.CASCADE)
     Image_category = models.ForeignKey('Category', on_delete=models.CASCADE)
 

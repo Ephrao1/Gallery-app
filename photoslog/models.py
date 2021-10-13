cc06326db2df1fe62c8340d90102c68f354abb3b
Ephraim Bundi <ebundi04@gmail.com>
2021-10-13 12:16:57 +0300

    Initial commit

--- photoslog/models.py
+++ photoslog/models.py
@@ -3,6 +3,7 @@ import datetime as dt
 from django.db import models
 from cloudinary.models import CloudinaryField
 
+
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length=50)
@@ -55,7 +56,8 @@ class Category(models.Model):
 class Image(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
-    Image = models.ImageField(upload_to='photos/')
+    # Image = models.ImageField(upload_to='photos/') 
+    image = CloudinaryField('image')
     Image_location = models.ForeignKey('Location', on_delete=models.CASCADE)
     Image_category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
